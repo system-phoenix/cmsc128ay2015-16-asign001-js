513152bdc27ffbb1f9af26a3059f744b384a8eba
system-phoenix <dmorias@up.edu.ph>
2016-02-14 01:03:45 +0800

    Added toNum function, wordsToNum dependency function.

--- script.js
+++ script.js
@@ -100,3 +100,34 @@ function wordsToNum(input){
 	}
 }
 
+function toNum(num){
+	switch(num){
+		case 'one': return 1;
+		case 'two': return 2;
+		case 'three': return 3;
+		case 'four': return 4;
+		case 'five': return 5;
+		case 'six': return 6;
+		case 'seven': return 7;
+		case 'eight': return 8;
+		case 'nine': return 9;
+		case 'ten': return 10;
+		case 'eleven': return 11;
+		case 'twelve': return 12;
+		case 'thirteen': return 13;
+		case 'fourteen': return 14;
+		case 'fifteen': return 15;
+		case 'sixteen': return 16;
+		case 'seventeen': return 17;
+		case 'eighteen': return 18;
+		case 'ninteen': return 19;
+		case 'twenty': return 20;
+		case 'thirty': return 30;
+		case 'forty': return 40;
+		case 'fifty': return 50;
+		case 'sixty': return 60;
+		case 'seventy': return 70;
+		case 'eighty': return 80;
+		case 'ninety': return 90;
+	}
+}
